92238fd582e9ef3ce1e03659afc17172f13b895b
ub1n <bin951024@naver.com>
2019-10-31 17:41:38 +0900

    test용 카메라 추가

--- app/src/main/java/org/techtown/dailypicture/MainActivity.kt
+++ app/src/main/java/org/techtown/dailypicture/MainActivity.kt
@@ -1,12 +1,26 @@
 package org.techtown.dailypicture
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        goalAddbutton.setOnClickListener {
+            var intent= Intent(this,AddGoalActivity::class.java)
+            startActivityForResult(intent,2)
+        }
+        textView2.setOnClickListener {
+            var intent=Intent(this,GoalDetailActivity::class.java)
+            startActivityForResult(intent,2)
+        }
     }
+
+
+
 }
